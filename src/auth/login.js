47b8c3019eb530583e5f76b3ff6294a1653f25c7
DESKTOP-R260JEI\gedom <gedomazo27@gmail.com>
2024-01-03 13:30:29 +0700

    update cors

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,107 +1,112 @@
 import './login.css';
 import React, { useState, useEffect } from 'react'
-import { useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios';
 import Cookies from "universal-cookie";
 import { jwtDecode } from "jwt-decode";
 const Login = () => {
-	const navigate=	useNavigate();
+	const navigate = useNavigate();
 	const cookies = new Cookies();
 	const [username, setUsername] = useState("");
-	const [password,setPassword] =  useState("");
-	
+	const [password, setPassword] = useState("");
 
-	const handleUsername= (event) => {
-        const { value } = event.target;
-        setUsername(value);
-    }
+
+	const handleUsername = (event) => {
+		const { value } = event.target;
+		setUsername(value);
+	}
 
 	const handlePassword = (event) => {
-        const { value } = event.target;
-        setPassword(value);
-    }
+		const { value } = event.target;
+		setPassword(value);
+	}
 
-console.log(username);
+	console.log(username);
 
-	const handleLogin =() => {
+	const handleLogin = () => {
 		cookies.remove("jwt_token");
 		axios.post('https://chat-server-ub0t.onrender.com/v1/login', {
 			username: username,
 			password: password
-		  })
-		  .then(function (response) {
-			console.log(response.data.id_token);
-			const decoded= jwtDecode(response.data.id_token);
+		}, {
+			withCredentials: true,
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': 'https://chat-client-8ra3.onrender.com'
+			}
+		})
+			.then(function (response) {
+				console.log(response.data.id_token);
+				const decoded = jwtDecode(response.data.id_token);
 
-			cookies.set("jwt_token",response.data.id_token,{
-				expires: new Date(decoded.exp * 1000)
+				cookies.set("jwt_token", response.data.id_token, {
+					expires: new Date(decoded.exp * 1000)
+				});
+				navigate("/chatroom");
+
+			})
+			.catch(function (error) {
+				console.log(error);
 			});
-			navigate("/chatroom");
-
-		  })
-		  .catch(function (error) {
-			console.log(error);
-		  });
 
 	}
 
 
-    return (
+	return (
 		<section className="ftco-section">
-		<div className="container">
-			<div className="row justify-content-center">
-				<div className="col-md-6 text-center mb-5">
-					<h2 className="heading-section">Welcome</h2>
+			<div className="container">
+				<div className="row justify-content-center">
+					<div className="col-md-6 text-center mb-5">
+						<h2 className="heading-section">Welcome</h2>
+					</div>
 				</div>
-			</div>
-			<div className="row justify-content-center">
-				<div className="col-md-7 col-lg-5">
-					<div className="wrap">
-						<div className="login-wrap p-4 p-md-5">
-			      	<div className="d-flex">
-			      		<div className="w-100">
-			      			<h3 className="mb-4">Sign In</h3>
-			      		</div>
-								<div className="w-100">
-									<p className="social-media d-flex justify-content-end">
-										<a href="#" className="social-icon d-flex align-items-center justify-content-center"><span className="fa fa-facebook"></span></a>
-										<a href="#" className="social-icon d-flex align-items-center justify-content-center"><span className="fa fa-twitter"></span></a>
-									</p>
+				<div className="row justify-content-center">
+					<div className="col-md-7 col-lg-5">
+						<div className="wrap">
+							<div className="login-wrap p-4 p-md-5">
+								<div className="d-flex">
+									<div className="w-100">
+										<h3 className="mb-4">Sign In</h3>
+									</div>
+									<div className="w-100">
+										<p className="social-media d-flex justify-content-end">
+											<a href="#" className="social-icon d-flex align-items-center justify-content-center"><span className="fa fa-facebook"></span></a>
+											<a href="#" className="social-icon d-flex align-items-center justify-content-center"><span className="fa fa-twitter"></span></a>
+										</p>
+									</div>
 								</div>
-			      	</div>
-			      		<div className="form-group mt-3">
-			      			<input type="text" className="form-control" required onChange={handleUsername} value={username} placeholder='username'/>
-			      			{/* <label className="form-control-placeholder">Username</label> */}
-			      		</div>
-		            <div className="form-group">
-		              <input id="password-field" type="password" className="form-control" required onChange={handlePassword} value={password} placeholder='password'/>
-		              {/* <label className="form-control-placeholder" >Password</label> */}
-		              <span toggle="#password-field" className="fa fa-fw fa-eye field-icon toggle-password"></span>
-		            </div>
-		            <div className="form-group">
-		            	<button  className="form-control btn btn-primary rounded submit px-3" onClick={handleLogin}>Sign In</button>
-		            </div>
-		            <div className="form-group d-md-flex">
-		            	<div className="w-50 text-left">
-			            	<label className="checkbox-wrap checkbox-primary mb-0">Remember Me
-									  <input type="checkbox" />
-									  <span className="checkmark"></span>
+								<div className="form-group mt-3">
+									<input type="text" className="form-control" required onChange={handleUsername} value={username} placeholder='username' />
+									{/* <label className="form-control-placeholder">Username</label> */}
+								</div>
+								<div className="form-group">
+									<input id="password-field" type="password" className="form-control" required onChange={handlePassword} value={password} placeholder='password' />
+									{/* <label className="form-control-placeholder" >Password</label> */}
+									<span toggle="#password-field" className="fa fa-fw fa-eye field-icon toggle-password"></span>
+								</div>
+								<div className="form-group">
+									<button className="form-control btn btn-primary rounded submit px-3" onClick={handleLogin}>Sign In</button>
+								</div>
+								<div className="form-group d-md-flex">
+									<div className="w-50 text-left">
+										<label className="checkbox-wrap checkbox-primary mb-0">Remember Me
+											<input type="checkbox" />
+											<span className="checkmark"></span>
 										</label>
 									</div>
 									<div className="w-50 text-md-right">
 										<a href="#">Forgot Password</a>
 									</div>
-		            </div>
-		          <p className="text-center">Not a member? <a data-toggle="tab" href="#signup">Sign Up</a></p>
-		        </div>
-		      </div>
+								</div>
+								<p className="text-center">Not a member? <a data-toggle="tab" href="#signup">Sign Up</a></p>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section>
 
-    )
-  }
-  
-  export default Login;
-  
+	)
+}
+
+export default Login;
